e68261b8efe3eb17c6218afcfed4ebc917bf8b38
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-09-14 19:19:19 -0700

    Initial commit, getting intellij set up.

---
+++ WelcomeToJava/src/WelcomeToJava.java
@@ -0,0 +1,11 @@
+/**
+ * https://www.hackerrank.com/challenges/welcome-to-java
+ */
+
+public class WelcomeToJava {
+
+    public static void main(String []argv) {
+        System.out.println("Hello World.");
+        System.out.println("Hello Java.");
+    }
+}
