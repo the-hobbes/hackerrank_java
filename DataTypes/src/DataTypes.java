38c1aa7a621fad3c2915d44e987c7a1552c16ec7
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-09-21 09:20:17 +0200

    solution to java datatypes exercise

--- DataTypes/src/DataTypes.java
+++ DataTypes/src/DataTypes.java
@@ -13,10 +13,10 @@ public class DataTypes {
                 long x=sc.nextLong();
                 System.out.println(x+" can be fitted in:");
 
-                if(x >= -32768 && x <= 32767) System.out.println("* short");
-                if(x >= -2147483648 && x<= Integer.MAX_VALUE) System.out.println("* int");
-                if(x >= Math.pow(2, -63) && x<= Math.pow(2, 63)) System.out.println("* long");
                 if(x >= -128 && x <= 127) System.out.println("* byte");
+                if(x >= -32768 && x <= 32767) System.out.println("* short");
+                if(x >= (-Integer.MAX_VALUE - 1) && x<= Integer.MAX_VALUE) System.out.println("* int");
+                if(x >= (-Long.MAX_VALUE - 1) && x<= Long.MAX_VALUE) System.out.println("* long");
             }
             catch(Exception e) {
                 System.out.println(sc.next()+" can't be fitted anywhere.");
