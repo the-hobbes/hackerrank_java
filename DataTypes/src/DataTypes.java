c097b5a65a473560963622bea83c28ed602df07f
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-09-21 09:15:51 +0200

    initial commit on java datatypes exercise

---
+++ DataTypes/src/DataTypes.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/java-datatypes
+ */
+public class DataTypes {
+    public static void main(String []argh) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for(int i=0; i<t; i++) {
+            try {
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+
+                if(x >= -32768 && x <= 32767) System.out.println("* short");
+                if(x >= -2147483648 && x<= Integer.MAX_VALUE) System.out.println("* int");
+                if(x >= Math.pow(2, -63) && x<= Math.pow(2, 63)) System.out.println("* long");
+                if(x >= -128 && x <= 127) System.out.println("* byte");
+            }
+            catch(Exception e) {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+        }
+    }
+}
