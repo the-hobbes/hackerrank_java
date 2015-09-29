25672ec291070a8a48325fe25398652cbb1a2bf2
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-09-29 18:14:46 -0400

    Solution to bigdecimal problem. I wanted to use a hashmap, but who knew it was so hard to sort by value in a hashmap?

--- BigDecimal/src/BigDecimalSolution.java
+++ BigDecimal/src/BigDecimalSolution.java
@@ -4,27 +4,38 @@ import java.math.BigDecimal;
 /**
  * https://www.hackerrank.com/challenges/java-bigdecimal
  */
+
 public class BigDecimalSolution {
 
+    public static void main(String[] args) {
+        // setup variables
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s[] = new String[n+2];
 
-    public static void main (String[] args) {
-        //Input
-        Scanner sc= new Scanner(System.in);
-        int n=sc.nextInt();
-        String []s=new String[n+2];
+        // read in input
         for(int i=0;i<n;i++) {
-            s[i]=sc.next();
+            s[i] = sc.next();
         }
 
-        // cast, compare, and write back to s
-        for(int i=0; i<n; i++) {
+        for(int i=0;i<n;i++) {
+            int index =i;
 
+            // compare the value at [index] to the rest of the values in the string array
+            for(int j=i+1;j<n;j++) {
+                BigDecimal bi2 = new BigDecimal(s[j]); // casting each to a BigDecimal
+                BigDecimal bi3 = new BigDecimal(s[index]);
+
+                // 0 means == in compareTo(), 1 is > than
+                if(bi2.compareTo(bi3)>0)
+                    index =j;
+            }
+            String smallerNumber = s[index];
+            s[index] =s[i];
+            s[i] = smallerNumber;
         }
-
-        //Output
         for(int i=0;i<n;i++) {
             System.out.println(s[i]);
         }
-
     }
 }
